1c60b460b8ee71531b9f58e0d2007bacecbf97f8
EricSchrappe <e.schrappe@icloud.com>
2021-10-27 20:32:56 +0300

    Creating home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">
+        <img src="" alt="Rowing">
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Centered nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#" center>Reservation</b-nav-item>
+        <b-nav-item href="#" center>Boats</b-nav-item>
+        <b-nav-item href="#" center>Company</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right no-caret>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <b-icon icon="person-circle" aria-hidden="true"></b-icon>
+          </template>
+          <b-dropdown-item href="#">View/Modify Account</b-dropdown-item>
+          <b-dropdown-item href="#">See Reservations</b-dropdown-item>
+          <b-dropdown-item href="#">Notifications</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
---
+++ src/components/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <Navbar />
+  <div class="cut"></div>
+  <div class="container">
+    <div style="background: #F8F8FF -webkit-box-shadow: 4px 4px 18px 1px rgba(0,0,0,0.32); 
+        box-shadow: 4px 4px 18px 1px rgba(0,0,0,0.32); !important" class="jumbotron">
+      <div class="row">
+      <div class="col-lg-7">
+        <h1 class="space display-4">Rowing Boat</h1>
+        <p class="space">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, officia. Iure quis, enim delectus a aliquam deserunt beatae illo eaque?</p>
+        <button class="btn btn-primary">Contact -></button>
+      </div>
+      <div class="col-lg-5"></div>
+    </div>
+    </div>
+  </div>
+  <div class="container fix">
+    <div class="col-md-4 px-0">
+      <img class="img-fluid float-right" src="../../assets/phone.png" alt="Phone">
+    </div>
+  </div>
+  <div class="container">
+    <p class="footer">Copyright &copy; 2021</p>
+    <router-link class="footer" to="/privacy"> Privacy Policy</router-link>
+  </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '../Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<style scoped>
+
+.vertical-center {
+  min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+
+  display: flex;
+  align-items: center;
+}
+
+.fix{
+    position:fixed;
+    bottom:0px;
+    left:60%;
+}
+
+.space{
+  margin-bottom: 8%;
+}
+
+.cut {
+  background: url("../../assets/background.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: blur(3px);
+  width: 100%;
+  height: 65%;
+  clip-path: polygon(0 0, 100% 0, 100% 51%, 0 100%);
+}
+
+.footer{
+  display: inline-block;
+  vertical-align: top;
+  margin-top: 1%;
+  padding: 10px 10px 10px 10px;
+}
+
+</style>
