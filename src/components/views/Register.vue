a2c7743ebb1202942e4f28ac3280d630250f5914
Dorian77210 <dorian.terb@gmail.com>
2021-11-13 13:38:41 +0200

    Add the registeration

--- src/components/views/Register.vue
+++ src/components/views/Register.vue
@@ -1,67 +1,67 @@
 <template>
     <div class="container">
         <h2 class="center"><strong>Registration</strong></h2>
-        <form method="POST">
+        <form>
         <div class="row align-items-center justify-content-center">
             <div class="col align-self-start">
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Name">
+                    <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Name" v-model="user.firstname">
                 </div>
                 <div class="form-group">
                     <label for="lastName">Last name</label>
-                    <input type="text" class="form-control" id="lastName" aria-describedby="lastNameHelp" placeholder="Last name">
+                    <input type="text" class="form-control" id="lastName" aria-describedby="lastNameHelp" placeholder="Last name" v-model="user.lastname">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" v-model="user.email">
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <input type="password" class="form-control" id="password" placeholder="Password" v-model="user.password">
                 </div>
             </div>
             <div class="col align-self-start">
                 <div class="form-group">
                     <label for="age">Age</label>
-                    <input type="number" class="form-control" id="age" aria-describedby="nameHelp" placeholder="25">
+                    <input type="number" class="form-control" id="age" aria-describedby="nameHelp" placeholder="25" v-model="user.age">
                 </div>
                 <div class="form-group">
                     <label for="gender">Gender</label>
-                    <select class="form-control custom-select" id="gender">
-                        <option>Female</option>
-                        <option>Male</option>
-                        <option>Diverse</option>
+                    <select class="form-control custom-select" id="gender" v-model="user.gender">
+                        <option value="FEMALE">Female</option>
+                        <option value="MALE">Male</option>
+                        <option value="DIVERSE">Diverse</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="phone">Phone Number</label>
-                    <input type="tel" class="form-control" id="phone" aria-describedby="nameHelp" placeholder="+49 123 3456495">
+                    <input type="tel" class="form-control" id="phone" aria-describedby="nameHelp" placeholder="+49 123 3456495" v-model="user.phoneNumber">
                 </div>
                 <div class="form-group">
                     <label for="fitness">Fitness Level</label>
-                    <select class="form-control custom-select" id="fitness">
-                        <option>Low</option>
-                        <option>Moderate</option>
-                        <option>High</option>
+                    <select class="form-control custom-select" id="fitness" v-model="user.fitness">
+                        <option value="LOW">Low</option>
+                        <option value="MODERATE">Moderate</option>
+                        <option value="HIGH">High</option>
                     </select>
                 </div>
             </div>
             <div class="col align-self-start">
                 <div class="form-group">
                     <label for="skill">Skill Level</label>
-                    <select class="form-control custom-select" id="skill">
-                        <option>Low</option>
-                        <option>Moderate</option>
-                        <option>High</option>
+                    <select class="form-control custom-select" id="skill" v-model="user.skill_level">
+                        <option value="LOW">Low</option>
+                        <option value="MODERATE">Moderate</option>
+                        <option value="HIGH">High</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="ambitions">Ambitions</label>
-                    <select class="form-control custom-select" id="ambitions">
-                        <option>Low</option>
-                        <option>Moderate</option>
-                        <option>High</option>
+                    <select class="form-control custom-select" id="ambitions" v-model="user.ambitions">
+                        <option value="LOW">Low</option>
+                        <option value="MODERATE">Moderate</option>
+                        <option value="HIGH">High</option>
                     </select>
                 </div>
             </div>
@@ -78,14 +78,14 @@
             </div>
             <div class="col"></div>
         </div>
-        <button class="btn btn-success btn-block mt-5" type="submit" :disabled="disabled">Register</button>
+        <button class="btn btn-success btn-block mt-5" type="submit" @click="e => submit(e)" :disabled="disabled">Register</button>
         </form>
     </div>
 </template>
 
 <script>
 import Modul from '../Modul.vue'
-
+import axios from 'axios'
 
 export default {
     name: 'Register',
@@ -95,12 +95,44 @@ export default {
     data() {
         return {
             disabled: true,
+            user: {
+                firstname: '',
+                lastname: '',
+                email: '',
+                age: 0,
+                password: '',
+                gender: '',
+                phoneNumber: '',
+                fitness: '',
+                ambitions: '',
+                skill_level: ''
+            },
+            erorrMessage: '',
+            error: false
         }
     },
     methods: {
         checkboxToogled (){
             this.disabled = !this.disabled;
             
+        },
+        async submit(e) {
+            e.preventDefault()
+            const formData = new FormData()
+            for(const key in this.user)
+            {
+                formData.append(key, this.user[key])
+            }
+
+            const result = await axios({
+                method: 'POST',
+                url: 'http://localhost:5000/user',
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" }
+            })
+
+            console.log(result)
+            return false
         }
     }
     
