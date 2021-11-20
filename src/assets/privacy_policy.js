39d66c20d4c626fa88e7d9d32c5a20c79a0f11e6
EricSchrappe <e.schrappe@icloud.com>
2021-11-20 18:12:05 +0200

    Include the privacy policy

---
+++ src/assets/privacy_policy.js
@@ -0,0 +1,120 @@
+export const privacy_policy = `INFORMATION ON THE PROCESSING OF PERSONAL 
+DATA OF THE WWW.ROWINGBOAT.COM SITE.
+ 
+INFORMATION PURSUANT TO EU REGULATION 2016/679 ("GDPR")
+Rowing Boat respects and protects the privacy of its users.
+ 
+In order to strengthen the trust of our Users, we provide the information relating to 
+the processing of personal data provided by users who interact with the web services 
+of Rowing Boat.
+ 
+DATA CONTROLLER
+The Data Controller of personal data relating to users of the Site is Rowing Boat srl, 
+whose main office is in Ruusulankatu 5, Helsinki, Finland, e-mail 
+info@rowingboat.com.
+The data processor is Mr. Eric Schrappe, in his capacity as legal representative, 
+available from the Data Controller.
+ 
+PLACE OF DATA PROCESSING
+The data of internet users will be processed directly at the site of Rowing Boat, 
+exclusively by the persons in charge of the processing or by any persons in charge 
+of occasional maintenance operations. Your data may be processed by hosting 
+service providers, who will act in this way as data processors of Rowing Boat. For 
+further information, please contact the Data Controller.
+ 
+PURPOSES AND METHODS OF DATA PROCESSING
+The data is collected and used to allow navigation on the Site, for statistical 
+purposes, for interaction with support, to contact the User, for registration and 
+authentication.
+The personal data provided by users who submit requests for services or request 
+information are used only to perform such services, or the requested services, as 
+well as to reply to such requests for information.
+ 
+TYPES OF DATA PROCESSED
+Through the site, both personal data provided voluntarily by the user for requesting 
+information or services can be acquired, for example by completing and sending a 
+specific form, as well as anonymous navigation data.
+ 
+Navigation data: The computer systems and software procedures used to operate 
+this website acquire, during their normal operation, some personal data whose 
+transmission is implicit in the use of internet communication protocols. This is 
+information that is not collected to be associated with identified interested parties, but 
+which by their very nature could, through processing, allow users to be identified. 
+This category of data includes IP addresses or domain names of the computers used 
+by users who connect to the site, the URI (Uniform Resource Identifier) addresses of 
+the requested resources, the time of the request, the method used to submit the 
+request to the server, the size of the file obtained in response, the numerical code 
+indicating the status of the response given by the server and other parameters 
+relating to the operating system and the user's IT environment. These data are used 
+only to obtain anonymous statistical information on the use of the site and to check 
+its correct functioning and are deleted immediately after processing. The data could 
+be used to ascertain responsibility in case of hypothetical computer crimes against 
+the site.
+ 
+Data provided voluntarily by the user: The explicit and voluntary provision of 
+personal data by the User is necessary for access to certain services offered by 
+Rowing Boat through the website (e.g. registration form for access to reserved area 
+via personal account). The personal data processed are essentially identification 
+data. 
+ 
+OPTIONAL PROVISION OF DATA
+Apart from that specified for navigation data, the user is free to provide personal data 
+to request the services offered by the company. The compilation and sending of the 
+form (s) presented on the site, or the sending of emails to the Controller's e-mail 
+addresses by the User, entails the subsequent acquisition of the sender's address 
+and the other information contained in communication. Failure to provide them may 
+make it impossible to obtain the requested service.
+ 
+METHODS OF DATA PROCESSING
+The processing is carried out through automated tools (eg using electronic 
+procedures and media) for the time strictly necessary to achieve the purposes for 
+which the data were collected and in compliance with the regulations in force on the 
+subject. In addition to the Data Controller, in some cases, categories of employees 
+involved in the organization of the site (administrative, commercial, marketing, legal, 
+system administrators) may have access to the Data.
+ 
+RETENTION PERIOD
+The Data is kept for the time necessary to perform the service requested by the 
+User, or stated by the purposes described in this document, and the User can 
+always ask for the Data to be interrupted or deleted.
+ 
+TREATMENT OF PERSONAL DATA - DETAILS
+
+a. Contact the user
+Contact form (Rowing Boat): The User, by filling in the contact form with his own 
+data, consents to their use to respond to requests for information, quotes, or any 
+other nature indicated by the form header.
+Personal Data collected: surname, email, name, telephone number and various 
+types of Data as specified in the privacy policy of the service.
+ 
+b. Registration and authentication
+By registering or authenticating, the User allows Rowing Boat to identify him and 
+give him access to dedicated services. 
+
+Direct registration The User registers by filling out the registration form and 
+providing his Personal Data directly to Rowing Boat.
+Personal Data collected: name, last name, email, password, age, gender, phone 
+number.
+ 
+c. Statistics
+The services contained in this section allow the Data Controller to monitor and 
+analyze traffic data and are used to keep track of User behavior.
+Google Analytics (Google Inc.) Google Analytics is a web analysis service provided 
+by Google Inc. ("Google"). Google uses the Personal Data collected for the purpose 
+of tracing and examining the use of Rowing Boat, compiling reports and sharing 
+them with other services developed by Google. Google may use the Personal Data 
+to contextualize and personalize the advertisements of its own advertising network. 
+Personal Data collected: Cookies and Usage Data.
+CONSENT
+By using this website, the user gives his consent to the processing of his data, in 
+accordance with the provisions of this Notice. If the user does not intend to give his / 
+her consent to the processing, he / she must immediately stop browsing the Site. 
+Continuing browsing on the Site will be equivalent in all respects to the provision of 
+consent to the processing.
+
+ 
+UPDATES TO THE INFORMATION
+Rowing Boat may make changes to this Information, ensuring in any case the timely 
+updating of this website and allowing access to the various versions that have 
+occurred over time, which can be consulted at the same address.
+`
--- src/components/Modul.vue
+++ src/components/Modul.vue
@@ -1,38 +1,32 @@
 <template>
 <div>
-  <b-button :variant="variant" v-b-modal.modal-center>{{button_text}}</b-button>
-  <b-modal :id="'modal-center-'+id" centered :title="title" :hide-footer="true">
-    <div v-if="showReserve === true">
-      <div class="row align-items-center justify-content-between">
-        <div class="col mb-3 ml-2 align-self-center">
-          <b-avatar></b-avatar>
-        </div>
-        <div class="col my-4 align-self-center">
-          <p>Name</p>
-        </div>
-        <div class="col my-4 align-self-center">
-          <p>+359-123456</p>
-        </div>
-      </div>
-    </div>
-    <div v-else>
-      <p class="my-4">{{description}}</p>
-    </div>
+  <b-button :variant="variant" v-b-modal.modal-tall>{{button_text}}</b-button>
+  <b-modal id="modal-tall" centered :title="title" :hide-footer="true">
+      <p class="my-4 space">{{description}}</p>
   </b-modal>
 </div>
 </template>
 
 <script>
+import { privacy_policy } from '../assets/privacy_policy.js';
+
 export default {
     name: 'Modul',
     props: {
         button_text: String,
         title: String,
-        description: String,
         variant: String,
-        showReserve: Boolean,
-        id: String,
-    }
-    
+    },
+    data() {
+      return {
+        description: privacy_policy,
+      }
+    },
 }
 </script>
+
+<style scoped>
+  .space{
+    white-space: pre-wrap;
+  }
+</style>
